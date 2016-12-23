27f3417329975ec3be8ef01d55c30fe6ce868010
Maximilien Noal <noal.maximilien@gmail.com>
2016-12-23 09:45:27 +0100

    Added Intro text and file presence guard

--- RETour/Program.cs
+++ RETour/Program.cs
@@ -19,18 +19,29 @@ namespace RETour
             Int32 targetPID = 0;
             try
             {
+                PrintInfoAndWarning();
+
+                Console.ReadKey();
+
+                if(!File.Exists(_targetExe))
+                {
+                    Console.WriteLine(String.Format("RESIDENTEVIL.EXE introuvable dans le dossier {0} !", Environment.CurrentDirectory));
+                    Console.WriteLine("Appuyez sur une touche pour quitter... ");
+                    Console.ReadKey();
+                    return;
+                }
+
                 RemoteHooking.IpcCreateServer<IpcInterface>(ref ChannelName, WellKnownObjectMode.SingleCall);
                 string injectionLibrary = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "REHookLib.dll");
                 RemoteHooking.CreateAndInject(_targetExe, "", 0, InjectionOptions.DoNotRequireStrongName, injectionLibrary, injectionLibrary, out targetPID, ChannelName);
                 Console.WriteLine("Created and injected process {0}", targetPID);
-                Console.WriteLine("<Press any key to exit>");
                 while(true)
                 {
                     if (Process.GetProcessesByName("RESIDENTEVIL").Length <= 0)
                     {
                         Environment.Exit(0);
                     }
-                    Thread.Sleep(1000);
+                    Thread.Sleep(2000);
                 }
             }
             catch (Exception exception)
@@ -38,6 +49,32 @@ namespace RETour
                 Debug.WriteLine("There was an error while connecting to target:\r\n{0}", exception.ToString());
                 Debug.WriteLine("<Press any key to exit>");
             }
+        }
+
+        private static void PrintInfoAndWarning()
+        {
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("                 Resident Evil");
+            Console.WriteLine("             VERSION CD Francaise");
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("            www.abandonware - france.org");
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine(" Merci aux forumeurs sur le site http://re123.bplaced.net/ pour le wrapper DirectDraw ! ");
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("Commandes de base (au clavier) :");
+            Console.WriteLine(" Touches fléchées : se déplacer");
+            Console.WriteLine(" C ou Espace ou Entrée : Action/Choisir/Frapper/Tirer");
+            Console.WriteLine(" V : Courir");
+            Console.WriteLine(" B : Pointer/Viser");
+            Console.WriteLine(" Z : Inventaire");
+            Console.WriteLine(" A : Options du jeu (permet de choisir de jouer avec une manette)");
+            Console.WriteLine(" ALT + F4 : QUITTER LE JEU");
+
+            Console.WriteLine();
+            Console.WriteLine(@"/!\ Ne pas utiliser ALT-TAB, ni CTRL-ESC, ni la touche Windows en cours de jeu,");
+            Console.WriteLine(@"/!\ sous peine de devoir redémarrer le jeu");
+            Console.WriteLine();
+            Console.WriteLine("Appuyez sur une touche pour continuer... ");
         }
     }
 }
