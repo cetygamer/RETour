fbbbbda1624f2647f7e3344b4b5fb73f74afdc7c
Maximilien Noal <noal.maximilien@gmail.com>
2017-04-19 20:39:43 +0200

    launcher

---
+++ ResidentEvilLauncher/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Windows.Navigation;
+
+namespace ResidentEvilLauncher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public DelegateCommand PlayCommand { get; private set; }
+        public DelegateCommand ControlsCommand { get; private set; }
+        public DelegateCommand OptionsCommand { get; private set; }
+        public DelegateCommand QuitCommand { get; private set; }
+
+        public MainWindow()
+        {
+            PlayCommand = new DelegateCommand(PlayCommand_Execute);
+            ControlsCommand = new DelegateCommand(ControlsCommand_Execute);
+            OptionsCommand = new DelegateCommand(OptionsCommand_Execute);
+            QuitCommand = new DelegateCommand(QuitCommand_Execute);
+            InitializeComponent();
+        }
+
+        private void PlayCommand_Execute(object parameters)
+        {
+            string exeName = "RESIDENTEVIL.EXE";
+            string workDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            if(IsEnhancedResolutionModeEnabled())
+            {
+                exeName = "RESIDENTEVIL-1024.EXE";
+            }
+            string path = Path.Combine(workDir, exeName);
+            if(File.Exists(path))
+            {
+                ProcessStartInfo psInfo = new ProcessStartInfo(path);
+                psInfo.UseShellExecute = true;
+                MessageBox.Show("Ne pas faire Alt-Tab en cours de jeu, sous peine de devoir redémarrer le jeu");
+                Process.Start(psInfo);
+            }
+            else
+            {
+                MessageBox.Show(String.Format("{0} introuvable.", path));
+            }
+        }
+
+        private bool IsEnhancedResolutionModeEnabled()
+        {
+            return Properties.Settings.Default.EnhancedResolutionMode == true;
+        }
+
+        private void ControlsCommand_Execute(object parameters)
+        {
+            var controlsWindow = new ControlsWindow();
+            controlsWindow.Owner = this;
+            controlsWindow.ShowDialog();
+        }
+
+        private void OptionsCommand_Execute(object parameters)
+        {
+            var optionsWindow = new OptionsWindow();
+            optionsWindow.Owner = this;
+            optionsWindow.ShowDialog();
+        }
+
+        public void QuitCommand_Execute(object parameters)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+    }
+}
---
+++ ResidentEvilLauncher/OptionsWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ResidentEvilLauncher
+{
+    /// <summary>
+    /// Interaction logic for OptionsWindow.xaml
+    /// </summary>
+    public partial class OptionsWindow : Window
+    {
+        public bool OptionEnhanceModeEnabled
+        {
+            get { return (bool)GetValue(OptionEnhanceModeEnabledProperty); }
+            set { SetValue(OptionEnhanceModeEnabledProperty, value); }
+        }
+        public static readonly DependencyProperty OptionEnhanceModeEnabledProperty =
+            DependencyProperty.Register("OptionEnhanceModeEnabled", typeof(bool), typeof(OptionsWindow), new PropertyMetadata(Properties.Settings.Default.EnhancedResolutionMode));
+
+        public bool OptionEnhanceModeDisabled
+        {
+            get { return (bool)GetValue(OptionEnhanceModeDisabledProperty); }
+            set { SetValue(OptionEnhanceModeDisabledProperty, value); }
+        }
+        public static readonly DependencyProperty OptionEnhanceModeDisabledProperty =
+            DependencyProperty.Register("OptionEnhanceModeDisabled", typeof(bool), typeof(OptionsWindow), new PropertyMetadata(true));
+
+        public DelegateCommand SavePrefs { get; private set; }
+        public DelegateCommand CancelPrefs { get; private set; }
+        public DelegateCommand DisableEnhanceMode { get; private set; }
+        public DelegateCommand EnableEnhanceMode { get; private set; }
+
+        public OptionsWindow()
+        {
+            SavePrefs = new DelegateCommand(SavePrefs_Execute);
+            CancelPrefs = new DelegateCommand(CancelPrefs_Execute);
+            DisableEnhanceMode = new DelegateCommand(DisableEnhanceMode_Execute);
+            EnableEnhanceMode = new DelegateCommand(EnableEnhanceMode_Execute);
+            SetCurrentValue(OptionEnhanceModeEnabledProperty, Properties.Settings.Default.EnhancedResolutionMode);
+            SetCurrentValue(OptionEnhanceModeDisabledProperty, !Properties.Settings.Default.EnhancedResolutionMode);
+
+            InitializeComponent();
+        }
+
+        private void SavePrefs_Execute(object parameters)
+        {
+            Properties.Settings.Default.EnhancedResolutionMode = (bool)GetValue(OptionEnhanceModeEnabledProperty);
+            Properties.Settings.Default.Save();
+            Close();
+        }
+
+        private void CancelPrefs_Execute(object parameters)
+        {
+            Close();
+        }
+
+        private void DisableEnhanceMode_Execute(object parameters)
+        {
+            SetCurrentValue(OptionEnhanceModeEnabledProperty, false);
+            SetCurrentValue(OptionEnhanceModeDisabledProperty, true);
+        }
+
+        private void EnableEnhanceMode_Execute(object parameters)
+        {
+            SetCurrentValue(OptionEnhanceModeEnabledProperty, true);
+            SetCurrentValue(OptionEnhanceModeDisabledProperty, false);
+        }
+    }
+}
